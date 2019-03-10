0247a1fdf3ad7b8efe4a93785481b3404c8a2be8
Hubert Dumas <hubertd@gmail.com>
2019-03-10 14:29:48 +1100

    using my own db users - progress

---
+++ IdentityServer/Models/Is4context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Models
+{
+    public class Is4context : DbContext
+    {
+        public Is4context(DbContextOptions<Is4context> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+    }
+}
